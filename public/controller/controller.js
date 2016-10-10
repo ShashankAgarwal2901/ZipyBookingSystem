7a861cd05d69b4c5012c20ae2c52f6599a4ba718
ShashankAgarwal2901 <shashank.agarwal29197@gmail.com>
2016-10-10 15:23:50 +0530

    jquery not working , find way to manipulate view

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -1,5 +1,11 @@
 var app = angular.module('MyApp' , []);
 
-app.controller(function($scope , $http){
-	
+app.controller('MainController' , function($scope , $http){
+	console.log("Controller is working")
+	$scope.loginattempt = function(){
+		console.log("Button clicked")
+		$http.post('/button' , $scope.email).success(function(err,data){
+			console.log(data);
+		});
+	}
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,27 @@
+var mongoose = require('mongoose');
+var body = require('body-parser');
+var jsdom = require("jsdom"); 
+var $ = require('jquery')(jsdom.jsdom().defaultView);
+mongoose.connect('mongodb://localhost:27017/users');
+var schema = mongoose.Schema({
+	username:String,
+	password:String
+})
+var users=mongoose.model('users' , schema);
+console.log(users);
 
 module.exports = function(app){
-	app.get('/button' , function(req,res){
-		console.log('Some got in');
-
-	
-	});
+	app.post('/button' , function(req,res){
+		console.log('Node working for login button');
+		console.log(req.body.email);
+		users.find({username:req.body.email},function(err,data){
+				if (err){console.log(err);}
+				else if(data[0])
+				{
+					console.log(data);
+					$('.one').append("<h1>WOrkign</h1>"); /*jquery not working , must find way to append */
+				}
+		});
+	});	
 }
 
