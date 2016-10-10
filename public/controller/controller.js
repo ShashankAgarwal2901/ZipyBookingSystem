c3567a6ba8170f62f0c8781d649e3e26595fea56
ShashankAgarwal2901 <shashank.agarwal29197@gmail.com>
2016-10-10 16:34:42 +0530

    Angular Problems in controller

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -2,10 +2,15 @@ var app = angular.module('MyApp' , []);
 
 app.controller('MainController' , function($scope , $http){
 	console.log("Controller is working")
+	$scope.rest=[{email:'Email or Username'}];
+	console.log($scope.rest); /*ANGULAR PROBLEMS*/
 	$scope.loginattempt = function(){
 		console.log("Button clicked")
 		$http.post('/button' , $scope.email).success(function(err,data){
-			console.log(data);
-		});
-	}
+			if(data){
+				console.log("Confirmed User");
+				$scope.rest.push({password:'Password'})
+			}
+			else{console.log(err)}
+		});}
 });
--- routes/routes.js
+++ routes/routes.js
@@ -2,26 +2,30 @@ var mongoose = require('mongoose');
 var body = require('body-parser');
 var jsdom = require("jsdom"); 
 var $ = require('jquery')(jsdom.jsdom().defaultView);
-mongoose.connect('mongodb://localhost:27017/users');
+mongoose.connect('mongodb://localhost:27017/users' , function(err,data){
+	if (err){
+		console.log(err);
+	}
+});
 var schema = mongoose.Schema({
 	username:String,
 	password:String
 })
 var users=mongoose.model('users' , schema);
-console.log(users);
+console.log("connected to dB");
 
 module.exports = function(app){
 	app.post('/button' , function(req,res){
 		console.log('Node working for login button');
-		console.log(req.body.email);
 		users.find({username:req.body.email},function(err,data){
 				if (err){console.log(err);}
 				else if(data[0])
 				{
-					console.log(data);
-					$('.one').append("<h1>WOrkign</h1>"); /*jquery not working , must find way to append */
+					res.json({'res':true});
+					 /*jquery not working , must find way to append */
 				}
 		});
 	});	
 }
+
 
